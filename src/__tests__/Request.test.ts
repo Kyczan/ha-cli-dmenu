feac70832dc5ff815c9a0af43c7e71c8f3b00277
Krystian Kowal <krystian@kowal.pro>
2019-07-07 22:24:10 +0200

    Add test to Request class

--- src/__tests__/Request.test.ts
+++ src/__tests__/Request.test.ts
@@ -1,9 +1,23 @@
+import axios from 'axios';
 import Request from '../Request';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('Request', () => {
   it('should not crash', () => {
     const request = new Request();
 
     expect(request).toBeInstanceOf(Request);
+  });
+
+  it('should handle `sendRequest` call', async () => {
+    const expectedResponse = { status: 'ok' };
+    mockedAxios.get.mockResolvedValue(expectedResponse);
+
+    const request = new Request();
+    const [err, res] = await request.sendRequest('test');
+
+    expect(res).toEqual(expectedResponse);
   });
 });
