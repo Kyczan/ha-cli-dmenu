587c03d76bb4e02e576ec5bf00224738f2cf0d7f
Krystian Kowal <krystian@kowal.pro>
2019-07-08 23:48:42 +0200

    Add tests for Menu.ts

--- src/__tests__/Menu.test.ts
+++ src/__tests__/Menu.test.ts
@@ -1,9 +1,45 @@
-import Menu from '../Menu';
+import { Menu } from 'rofix';
+import AppMenu from '../Menu';
 
-describe('Menu', () => {
+jest.mock('rofix');
+
+describe('AppMenu', () => {
+  beforeEach(() => {
+    Menu.mockClear();
+  });
+
   it('should not crash', () => {
-    const menu = new Menu();
+    const appMenu = new AppMenu();
 
-    expect(menu).toBeInstanceOf(Menu);
+    expect(appMenu).toBeInstanceOf(AppMenu);
+  });
+
+  it('should handle `getOption` WITHOUT error', async () => {
+    const expectedResponse = 'test_webhook';
+    const payload = { stdout: expectedResponse };
+    Menu.mockImplementation(() => {
+      return {
+        open: () => Promise.resolve(JSON.stringify(payload)),
+      };
+    });
+
+    const appMenu = new AppMenu();
+    const [err, res] = await appMenu.getOption();
+
+    expect(res).toEqual(expectedResponse);
+  });
+
+  it('should handle `getOption` WITH error', async () => {
+    const expectedError = 'Error';
+    Menu.mockImplementation(() => {
+      return {
+        open: () => Promise.reject(expectedError),
+      };
+    });
+
+    const appMenu = new AppMenu();
+    const [err, res] = await appMenu.getOption();
+
+    expect(err).toEqual(expectedError);
   });
 });
